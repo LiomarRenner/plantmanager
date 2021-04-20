92255208f3f9647e472d73912ae4f845a666ce8d
LiomarRenner <liomarrenner@gmail.com>
2021-04-20 14:47:12 -0300

    WelcomeScreenDone

--- App.tsx
+++ App.tsx
@@ -1,19 +1,21 @@
 //import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import AppLoading from 'expo-app-loading';
 import { Welcome } from './src/screens/Welcome';
+import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
 export default function App() {
+  const [ fontsLoaded ] = useFonts({
+    Jost_400Regular, 
+    Jost_600SemiBold
+  });
+
+  if(!fontsLoaded)
+    return <AppLoading/>
+
   return (
     <Welcome />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
