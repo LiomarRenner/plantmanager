93da5aa58c35091f91ab65752c08167959ed0ba2
LiomarRenner <liomarrenner@gmail.com>
2021-04-21 17:11:49 -0300

    ConfirmationScreen

--- App.tsx
+++ App.tsx
@@ -2,7 +2,8 @@
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 //import { Welcome } from './src/screens/Welcome';
-import { UserIdentification } from './src/screens/UserIdentification';
+//import { UserIdentification } from './src/screens/UserIdentification';
+import { Confirmation } from './src/screens/Confirmation';
 import { useFonts, Jost_400Regular, Jost_600SemiBold } from '@expo-google-fonts/jost';
 
 export default function App() {
@@ -16,7 +17,8 @@ export default function App() {
 
   return (
     //<Welcome />
-    <UserIdentification/>
+    //<UserIdentification/>
+    <Confirmation/>
   );
 }
 
---
+++ src/screens/Confirmation/index.tsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    Text,
+    View,
+    StyleSheet
+}from 'react-native';
+
+import { Button } from '../../components/Button';
+import colors from '../../../styles/colors';
+import fonts from '../../../styles/fonts';
+
+export function Confirmation(){
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.content}>
+                <Text style={styles.emoji}>
+                    {'😎'}
+                </Text>
+
+                <Text style={styles.text}>
+                    Prontinho
+                </Text>    
+
+                <Text style={styles.subText}>
+                    Agora vamos começar a cuidar das suas plantinhas
+                    com muito cuidado
+                </Text>
+            </View>
+
+            <View style={styles.footer}>
+                <Button />
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        alignItems:'center',
+        justifyContent:'space-around',
+
+    },
+    content: {
+        flex:1,
+        justifyContent:'center',
+        width:'100%',
+        padding:30,
+
+    },
+    emoji: {
+        fontSize: 38,
+        textAlign: 'center'
+    },
+    text: {
+        fontSize:24,
+        textAlign:'center',
+        color: colors.heading,
+        fontFamily: fonts.heading,
+        marginTop: 15,
+        lineHeight: 38,
+    },
+    subText: {
+        fontSize:18,
+        textAlign:'center',
+        color: colors.heading,
+        fontFamily: fonts.text,
+        paddingVertical: 10,
+    },
+    footer: {
+        marginTop:40,
+        width:'100%',
+        paddingHorizontal: 40,
+        marginBottom:180,
+
+    }
+})
+
