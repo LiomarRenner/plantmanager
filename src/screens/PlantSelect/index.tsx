40960a17baebc473676458f27f02cafa0ee6616f
LiomarRenner <liomarrenner@gmail.com>
2021-04-23 15:45:27 -0300

    PlantSelectScreenNewComponents
    Add two components(PlantCardPrimary && EnvironmentButton) and connect to the API.

--- src/screens/PlantSelect/index.tsx
+++ src/screens/PlantSelect/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import {
     View,
     Text,
@@ -8,10 +8,60 @@ import {
 
 import colors from '../../../styles/colors';
 import fonts from '../../../styles/fonts';
-import { EnviromentButton } from '../../components/EnviromentButton';
+
+import { EnvironmentButton } from '../../components/EnvironmentButton';
 import { Header } from '../../components/Header';
+import { PlantCardPrimary } from '../../components/PlantCardPrimary';
+
+import api from '../../services/api';
+interface EnvironmentProps { 
+    key: string;
+    title: string;
+}
+
+interface PlantProps { 
+    id: string;
+    name: string;
+    about: string;
+    water_tips: string;
+    photo: string;
+    environments: [string]; 
+    frequency: {
+        times: number;
+        repeat_every: string;
+    } 
+}
 
 export function PlantSelect(){
+    const [environments, setEnvironments] = useState<EnvironmentProps[]>([]);
+    const [plants, setPlants] = useState<PlantProps[]>([]);
+
+    useEffect(() => {
+        async function fetchEnvironment(){
+            const { data } = await api.get('plants_environments');
+            setEnvironments([
+                {
+                    key:'all',
+                    title:'Todos',
+                },
+                ...data
+            ]);
+        }
+
+        fetchEnvironment();
+    },[])
+
+    useEffect(() => {
+        async function fetchPlants(){
+            const { data } = await api.get('plants');
+            setPlants(data);
+        }
+
+        fetchPlants();
+    },[])
+
+
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>    
@@ -23,13 +73,25 @@ export function PlantSelect(){
 
             <View>
                 <FlatList 
-                    data={[1,2,3,4,5]}
+                    data={environments}
                     renderItem={({ item }) => (
-                        <EnviromentButton title="Cozinha" active/>
+                        <EnvironmentButton title={item.title} />
                     )}
                     horizontal
                     showsHorizontalScrollIndicator={false}
-                    contentContainerStyle={styles.enviromentList}
+                    contentContainerStyle={styles.environmentList}
+                />
+            </View>
+
+            <View style={styles.plants}>
+                <FlatList 
+                    data={plants}
+                    renderItem={({ item }) => (
+                        <PlantCardPrimary
+                            data={item}
+                        />    
+                    )}
+                    showsVerticalScrollIndicator={false}
                 />
             </View>
 
@@ -59,11 +121,16 @@ const styles = StyleSheet.create({
         color: colors.heading,
         fontFamily: fonts.text,
     },
-    enviromentList: {
+    environmentList: {
         height: 40,
         justifyContent:'center',
         paddingBottom:5,
         marginLeft:22,
         marginVertical:32,
+    },
+    plants: {
+        flex:1,
+        paddingHorizontal:32,
+        justifyContent:'center',
     }
 });
