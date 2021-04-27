2df12c00d480c1f832225c225425abfb6f04677f
LiomarRenner <liomarrenner@gmail.com>
2021-04-27 00:32:53 -0300

    PassDataBetweenScreens

--- src/screens/PlantSave/index.tsx
+++ src/screens/PlantSave/index.tsx
@@ -10,27 +10,45 @@ import {
     TouchableOpacity,
 }from 'react-native';
 import { SvgFromUri } from 'react-native-svg';
+import { useRoute } from '@react-navigation/core';
 
 import waterdrop from '../../assets/waterdrop.png';
 import colors from '../../../styles/colors';
 import fonts from '../../../styles/fonts';
 import { Button } from '../../components/Button';
 import { getBottomSpace } from 'react-native-iphone-x-helper';
+interface Params {
+    plant: {
+        id: string;
+        name: string;
+        about: string;
+        water_tips: string;
+        photo: string;
+        environments: [string]; 
+        frequency: {
+            times: number;
+            repeat_every: string;
+        }
+    }
+}
 
 export function PlantSave(){
+    const route = useRoute();
+    const { plant} = route.params as Params; 
+
     return (
         <View style={styles.container}>
 
         <View style={styles.plantInfo}>
             <SvgFromUri
-                uri=""
+                uri={ plant.photo }
                 height={150}
                 width={150}
+
             />
 
-            <Text style={styles.plantName}> Nome da Planta </Text>  
-            <Text style={styles.plantAbout}> Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Molestias accusantium dolorum delectus. 
+            <Text style={styles.plantName}> { plant.name} </Text>  
+            <Text style={styles.plantAbout}> { plant.about } 
                 
             </Text> 
         </View>
@@ -42,7 +60,7 @@ export function PlantSave(){
                     style={styles.tipImage}
                 />
                 <Text style={styles.tipText}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    { plant.water_tips}
                 </Text>
             </View>
 
@@ -123,6 +141,9 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         backgroundColor: colors.blue_light,
         padding: 20,
+        borderRadius: 14,
+        position:'relative',
+        bottom: 60,
 
     },
 })
